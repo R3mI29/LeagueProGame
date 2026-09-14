75e162926de036b1be9440e834efb819fbb8126b
r3mI29 <rmillot864@outlook.fr>
2026-09-15 00:08:25 +0200

    modif pour les games en local

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,8 +1,9 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('https://brown-snakes-smile.loca.lt', {
-  extraHeaders: {
-    "Bypass-Tunnel-Reminder": "true"
-  },
-  transports: ["websocket"]
+// Récupère dynamiquement l'IP locale ou localhost
+const SERVER_IP = window.location.hostname;
+const URL = `http://${SERVER_IP}:3001`;
+
+export const socket = io(URL, {
+  autoConnect: true
 });
