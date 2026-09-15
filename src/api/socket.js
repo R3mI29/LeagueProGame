60ef27ce1209cbd9f1036a9be46058fdfd788270
VirgileKR <jglgap77@gmail.com>
2026-09-15 20:30:38 +0200

    add tunnel

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,8 +1,11 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('https://brown-snakes-smile.loca.lt', {
+const serverUrl = window.location.hostname.includes('loca.lt') 
+  ? 'https://all-baboons-lose.loca.lt' // <--- Mets ton URL actuelle du tunnel ici
+  : 'http://${SERVER_IP}:3001';
+
+export const socket = io(serverUrl, {
   extraHeaders: {
     "Bypass-Tunnel-Reminder": "true"
-  },
-  transports: ["websocket"]
+  }
 });
